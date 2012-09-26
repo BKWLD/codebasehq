d70f7549dc24754033692829e98eb1609fbf374b
Robert Reinhard <robert.reinhard@bkwld.com>
2012-09-25 17:57:57 -0700

    Commenting the config file

--- config/codebasehq.php
+++ config/codebasehq.php
@@ -1,5 +1,10 @@
 <?php
 
 return array(
+	
+	/* 
+	 * The API key for your Codebase project.  You can get this by visiting 
+	 * the Exceptions view of a Codebase project, you'll see it displayed on the page. 
+	 */
 	'api_key' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
 );
