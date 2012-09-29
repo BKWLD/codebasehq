6ffbfa405ce3523a2c2ee54c958e5ee99c43e5bb
Robert Reinhard <robert.reinhard@bkwld.com>
2012-09-28 22:42:01 -0700

    Fixing an omission in config path

--- start.php
+++ start.php
@@ -26,7 +26,7 @@ IoC::singleton('airbrake', function() {
 // Listen for exception events and pass them to Codebase HQ.  Using this rather than
 // listening to log events so we can get the full exception stack trace rather than the
 // summary that gets truncated by exception_line()
-if (Config::get('laravel-plus-codebase::enable')
+if (Config::get('laravel-plus-codebase::codebase.enable')
 	&& Config::get('error.log')) {
 	Config::set('error.logger', function($exception) {
 
