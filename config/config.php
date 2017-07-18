496367b6aaafed403057be1df21455f37af29e00
Robert Reinhard <robert.reinhard@bkwld.com>
2017-07-18 08:56:20 -0700

    Reorganizing repo

--- config/config.php
+++ config/config.php
@@ -1,28 +1,28 @@
 <?php return array(
-	
+
 	/**
-	 * The CodebaseHQ project associated with this Laravel app.  This is the short 
+	 * The CodebaseHQ project associated with this Laravel app.  This is the short
 	 * name that is used in the URL of the CodebaseHQ project.
 	 */
 	'project' => 'xxxxxx',
-	
+
 	/**
-	 * Your user CodebaseHQ API credentials.  These are displayed on the 
-	 * 'Settings > My Profile' page within the CodebaseHQ site. It is recommended 
+	 * Your user CodebaseHQ API credentials.  These are displayed on the
+	 * 'Settings > My Profile' page within the CodebaseHQ site. It is recommended
 	 * that you do not hard code them, but load load them from your environment.
 	 */
 	'api' => array(
 		'username' => @$_ENV['CODEBASE_USERNAME'],
 		'key' => @$_ENV['CODEBASE_KEY'],
 	),
-	
-	/* 
-	 * The Exceptions API key for your Codebase project.  You can get this by 
-	 * visiting  the Exceptions view of a Codebase project, you'll see it 
-	 * displayed on the page. 
+
+	/*
+	 * The Exceptions API key for your Codebase project.  You can get this by
+	 * visiting  the Exceptions view of a Codebase project, you'll see it
+	 * displayed on the page.
 	 */
 	'exceptions_key' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-	
+
 	/*
 	 * Toggle to turn off exception logging to codebase. This can be overriden in
 	 * enviornment specific config files
--- src/config/local/config.php
+++
@@ -1,9 +0,0 @@
-<?php return array(
-
-	/*
-	 * Most people will probably want to turn off exception logging
-	 * while doing local development
-	 */
-	'exception_logging' => false,
-	
-);
